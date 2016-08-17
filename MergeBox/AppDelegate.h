84aeab250a3a7361264293a6ee3197ce49a9c447
Mayur Joshi <mayur@azoi.com>
2016-08-17 11:28:54 +0530

    Initial Commit

---
+++ MergeBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MergeBox
+//
+//  Created by Mayur Joshi on 17/08/16.
+//  Copyright © 2016 Mayur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MergeBox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MergeBox
+//
+//  Created by Mayur Joshi on 17/08/16.
+//  Copyright © 2016 Mayur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
